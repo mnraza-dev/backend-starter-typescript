84b25bb1116f7db7d775cbccd6e2710c5ff84a74
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-19 14:49:05 +0530

    add CORS middleware and .env.sample file for environment variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,16 @@
 import app from "./server";
+import cors from "cors";
 
-const port = 3000;
+import dotenv from "dotenv";
+dotenv.config();
+
+app.use(cors(
+    {
+        origin: process.env.CLIENT_URL
+    }
+));
+
+const port = process.env.PORT || 3000;
 app.listen(3000, () => {
     console.log(` ✨ Server started at http://localhost:${port}`);
 });
